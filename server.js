824f3cf98a064f8a2a74171c4f63c2850e33bde6
Parth-Chikalkar <parthchikalkar56@gmail.com>
2025-01-25 19:30:27 +0530

    hosted

--- server.js
+++ server.js
@@ -15,9 +15,7 @@ const PORT = process.env.PORT || 3000;
 app.set("view engine","ejs");
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-app.get('/',(req,res) =>{
-    res.send("td sd adhuri kahani");
-})
+
 const isLoggedin = (req, res, next) => {
     if (!req.cookies.token) {
         return res.redirect("/login");
